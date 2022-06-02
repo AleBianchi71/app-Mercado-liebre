25f4613aa08608a5aa30b87ad2b5f4cac103c6bf
AleBianchi71 <bianchialejandro998@gmail.com>
2022-06-02 12:34:13 -0300

    subiendo

--- app.js
+++ app.js
@@ -6,11 +6,9 @@ const app = express();
 const publicPath = path.resolve(__dirname,"./public");
 app.use(express.static(publicPath));
 
-const port = (process.env.PORT || 3000);
-
-app.listen(port,()=> {
-    console.log('servidor corriendo en el puerto 3000');
-});
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening.. :)" );
+  });
 
 app.get("/",(req,res)=>{
     res.sendFile(path.resolve(__dirname,"./views/home.html"));
